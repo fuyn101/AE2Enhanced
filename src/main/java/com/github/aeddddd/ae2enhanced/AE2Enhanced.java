e8119a1f1a57bef246dec6cac09035b1295cff9b
aeddddd <Sy.aeddddd@gmail.com>
2026-05-12 19:17:29 +0800

    feat: add crafting recipes for universal buses and bump version to 1.4.0-dev
    - Recipe for PartUniversalImportBus: logic processor + annihilation core
      + iron ingots + sticky piston
    - Recipe for PartUniversalExportBus: logic processor + formation core
      + iron ingots + piston
    - Version bump: 1.3.3 -> 1.4.0-dev in AE2Enhanced.java and build.gradle

--- src/main/java/com/github/aeddddd/ae2enhanced/AE2Enhanced.java
+++ src/main/java/com/github/aeddddd/ae2enhanced/AE2Enhanced.java
@@ -49,7 +49,7 @@ public class AE2Enhanced {
 
     public static final String MOD_ID = "ae2enhanced";
     public static final String MOD_NAME = "AE2Enhanced";
-    public static final String VERSION = "1.3.3";
+    public static final String VERSION = "1.4.0-dev";
 
     public static final String CLIENT_PROXY = "com.github.aeddddd.ae2enhanced.proxy.ClientProxy";
     public static final String SERVER_PROXY = "com.github.aeddddd.ae2enhanced.proxy.CommonProxy";
