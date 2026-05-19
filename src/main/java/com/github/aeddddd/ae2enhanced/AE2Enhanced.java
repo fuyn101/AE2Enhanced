472f12c0ff5dc8d2fcb1aba8c001af2843fb4ce1
aeddddd <Sy.aeddddd@gmail.com>
2026-05-19 15:37:25 +0800

    Bump version to 1.4.3-beta-hotfix1

--- src/main/java/com/github/aeddddd/ae2enhanced/AE2Enhanced.java
+++ src/main/java/com/github/aeddddd/ae2enhanced/AE2Enhanced.java
@@ -51,7 +51,7 @@ public class AE2Enhanced {
 
     public static final String MOD_ID = "ae2enhanced";
     public static final String MOD_NAME = "AE2Enhanced";
-    public static final String VERSION = "1.4.3-beta";
+    public static final String VERSION = "1.4.3-beta-hotfix1";
 
     public static final String CLIENT_PROXY = "com.github.aeddddd.ae2enhanced.proxy.ClientProxy";
     public static final String SERVER_PROXY = "com.github.aeddddd.ae2enhanced.proxy.CommonProxy";
