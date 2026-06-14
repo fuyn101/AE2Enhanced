31666b51140569bddf6a96a3adfd931b9fe58d1a
aeddddd <Sy.aeddddd@gmail.com>
2026-06-14 20:09:44 +0800

    Bump version to 1.5.3-hotfix2

--- src/main/java/com/github/aeddddd/ae2enhanced/AE2Enhanced.java
+++ src/main/java/com/github/aeddddd/ae2enhanced/AE2Enhanced.java
@@ -36,7 +36,7 @@ public class AE2Enhanced {
 
     public static final String MOD_ID = "ae2enhanced";
     public static final String MOD_NAME = "AE2Enhanced";
-    public static final String VERSION = "1.5.3-hotfix1";
+    public static final String VERSION = "1.5.3-hotfix2";
 
     public static final String CLIENT_PROXY = "com.github.aeddddd.ae2enhanced.proxy.ClientProxy";
     public static final String SERVER_PROXY = "com.github.aeddddd.ae2enhanced.proxy.CommonProxy";
